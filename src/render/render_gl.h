2fa37ac2611752263bdacd20a8b3fc298fb39564
ramakarl <ramahoetzlein@gmail.com>
2025-07-05 19:20:52 -0700

    update

--- src/render/render_gl.h
+++ src/render/render_gl.h
@@ -1,6 +1,6 @@
 
 //-------------------------
-// Copyright 2025 (c) Quanta Sciences, Rama Hoetzlein
+// Copyright 2020-2025 (c) Quanta Sciences, Rama Hoetzlein
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
